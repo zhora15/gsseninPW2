6fa64f088c0dedd3afdd3527aeec0011899ffb02
Georgy Senin <georgy.senin@gmail.com>
2022-10-08 15:39:56 +0300

    Initial Commit

---
+++ gsseninPW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gsseninPW2
+//
+//  Created by Георгий Сенин on 08.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
