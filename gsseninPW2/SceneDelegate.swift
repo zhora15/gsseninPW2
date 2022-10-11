448bd268359831ebf669ae8da5a7ebee21a85175
Georgy Senin <georgy.senin@gmail.com>
2022-10-11 15:18:03 +0300

    Initial commit

--- gsseninPW2/SceneDelegate.swift
+++ gsseninPW2/SceneDelegate.swift
@@ -8,45 +8,145 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
-
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options сonnectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene)
+        else { return }
+    
+        let window = UIWindow(windowScene: windowScene)
+        let navigationController = UINavigationController(rootViewController: WelcomeViewController())
+        window.rootViewController = navigationController
+        self.window = window
+        window.makeKeyAndVisible()
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
 
+final class WelcomeViewController: UIViewController {
+    private let commentLabel = UILabel()
+    private let valueLabel = UILabel()
+    private var value: Int = 0
+    let incrementButton = UIButton (type: .system)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .lightGray
+        setupIncrementButton()
+        setupValueLabel()
+        setupCommentView()
+        setupMenuButtons()
+    }
+    
+    private func makeMenuButton(title: String) -> UIButton {
+        let button = UIButton()
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 12
+        button.titleLabel?.font = .systemFont(ofSize: 16.0, weight: .medium)
+        button.backgroundColor = .white
+        button.heightAnchor.constraint(equalTo:
+        button.widthAnchor).isActive = true
+        return button
+    }
+    
+    private func setupMenuButtons() {
+        let colorsButton = makeMenuButton(title: "🎨")
+        let notesButton = makeMenuButton(title: "📝")
+        let newsButton = makeMenuButton(title: "📰")
+            
+        let buttonsSV = UIStackView(arrangedSubviews: [colorsButton, notesButton, newsButton])
+        buttonsSV.spacing = 12
+        buttonsSV.axis = .horizontal
+        buttonsSV.distribution = .fillEqually
+        self.view.addSubview(buttonsSV)
+        buttonsSV.pin(to: self.view, [.left: 24, .right: 24])
+        buttonsSV.pinBottom(to: self.view.safeAreaLayoutGuide.bottomAnchor, 24)
+    }
+                                      
+    @discardableResult
+    private func setupCommentView() -> UIView {
+        let commentView = UIView()
+        commentView.backgroundColor = .white
+        commentView.layer.cornerRadius = 12
+        view.addSubview(commentView)
+        commentView.pinTop(to: self.view.safeAreaLayoutGuide.topAnchor)
+        commentView.pin(to: self.view, [.left: 24, .right: 24])
+        commentLabel.font = .systemFont(ofSize: 14.0, weight: .regular)
+        commentLabel.textColor = .systemGray
+        commentLabel.numberOfLines = 0
+        commentLabel.textAlignment = .center
+        commentView.addSubview(commentLabel)
+        commentLabel.pin(to: commentView, [.top: 16, .left: 16, .bottom: 16, .right: 16])
+        commentLabel.text = "Tap!"
+        return commentView
+    }
+    
+    private func setupValueLabel() {
+        valueLabel.font = .systemFont(ofSize: 40.0, weight: .bold)
+        valueLabel.textColor = .black
+        valueLabel.text = "\(self.value)"
+        self.view.addSubview(valueLabel)
+        valueLabel.pinBottom(to: incrementButton.topAnchor, 16)
+        valueLabel.pinCenterX(to: self.view.centerXAnchor)
+    }
+    
+    private func setupIncrementButton() {
+        self.view.addSubview(incrementButton)
+        incrementButton.setTitle("Increment", for: .normal)
+        incrementButton.setTitleColor(.black, for: .normal)
+        incrementButton.layer.cornerRadius = 12
+        incrementButton.titleLabel?.font = .systemFont(ofSize: 16.0, weight: .medium)
+        incrementButton.backgroundColor = .white
+        //incrementButton.layer.applyShadow()
+        self.view.addSubview(incrementButton)
+        incrementButton.setHeight(48)
+        incrementButton.pinTop(to: self.view.centerYAnchor)
+        incrementButton.pin(to: self.view, [.left: 24, .right: 24])
+        incrementButton.addTarget(self, action: #selector(incrementButtonPressed), for: .touchUpInside)
+    }
+    
+    func getCommentLabel(value: Int) -> String{
+        switch value {
+            case 0...10:
+                return "1"
+            case 10...20:
+                return "2"
+            case 20...30:
+                return "3"
+            case 30...40:
+                return "4"
+            case 40...50:
+                return "🎉🎉🎉🎉🎉🎉🎉🎉🎉"
+            case 50...60:
+                return "big boy"
+            case 60...70:
+                return "70 70 70 moreeeee"
+            case 70...80:
+                return "⭐ ⭐ ⭐ ⭐ ⭐ ⭐ ⭐ ⭐ ⭐ "
+            case 80...90:
+                return "80+\n go higher!"
+            case 90...100:
+                return "100!! to the moon!!"
+            default:
+                return "100!! to the moon!!"
+        }
+    }
+    
+    func updateUI() {
+        valueLabel.text = "\(self.value)"
+        if getCommentLabel(value: self.value) != self.commentLabel.text{
+            UIView.transition(with: commentLabel, duration: 0.5, options: .transitionFlipFromBottom, animations: {
+                self.commentLabel.text = self.getCommentLabel(value: self.value)
+            })
+        }
+    }
+    
+    @objc
+    func incrementButtonPressed() {
+        self.value += 1
+        self.updateUI()
+    }
+}
